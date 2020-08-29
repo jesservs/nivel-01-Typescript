2e3cf7770efdbfc51313c1c9071787f2c2df2551
jesservs <jesservs@hotmail.com>
2020-08-28 23:01:56 -0300

    chore: Initial commit

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+export function helloWorld(request: Request, response: Response) {
+    const user  = createUser({
+        email: 'teste@teste',
+        password:'teste123',
+        techs: [
+            'nodejs',
+            'react',
+            { title:'Javascript', experience:100 },
+        ]
+    });
+    return response.json({message: 'Hello World'});
+}
